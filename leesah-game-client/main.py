d875bde71399a41add337d44d46208f924f5382a
ulrikah <ulrikhalmoy@gmail.com>
2021-10-13 15:32:17 +0200

    MVP av spørsmål og svar med JSON-serialisering

--- leesah-game-client/main.py
+++ leesah-game-client/main.py
@@ -1,20 +1,36 @@
 from kafka import KafkaConsumer, KafkaProducer
-from random import random
+import json
 
 TEAM_ID = "leesah-team-1"
-TOPIC = "quiz-rapid"
+QUIZ_TOPIC = "quiz-rapid"
+UTF_8 = "utf-8"
+
+
+def parse_kafka_message(msg: bytes) -> dict:
+    return json.loads(msg.value.decode(UTF_8))
+
 
 if __name__ == "__main__":
     consumer = KafkaConsumer(
-        TOPIC,
+        QUIZ_TOPIC,
         bootstrap_servers="localhost:29092",
         group_id=TEAM_ID,
         auto_offset_reset="earliest",
     )
     producer = KafkaProducer(bootstrap_servers="localhost:29092")
     for msg in consumer:
-        parsed_msg = msg.value.decode()
-        print(parsed_msg)
-        if parsed_msg == "challenge1":
-            print("SVARER PÅ CHALLENGE")
-            producer.send(TOPIC, b"answer to challenge1")
+        try:
+            parsed_msg = parse_kafka_message(msg)
+            # TODO: use proper schema matching when parsing JSON
+            if parsed_msg["type"] == "question":
+                svar = {"type": "svar", "innhold": "1+1=2"}
+                print("SVARER MED:", svar)
+                producer.send(QUIZ_TOPIC, bytes(json.dumps(svar), UTF_8))
+            elif parsed_msg["type"] == "svar":
+                print("MOTTOK SVAR:", parsed_msg)
+            else:
+                print("Fikk inn melding som verken var spørsmål eller svar", parsed_msg)
+        except json.JSONDecodeError:
+            print("Unable to parse JSON:", msg.value)
+        except KeyError as e:
+            print("JSON følger ikke skjemaet", msg.value, e)
