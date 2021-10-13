bff5ed012211568b340387c3c39377c2676c02ff
ulrikah <ulrikhalmoy@gmail.com>
2021-10-13 16:07:04 +0200

    Bruker skjemavalidering for å validere kafkameldingene

--- leesah-game-client/main.py
+++ leesah-game-client/main.py
@@ -1,12 +1,17 @@
 from kafka import KafkaConsumer, KafkaProducer
+from schema import Schema, Or, Use, SchemaError
 import json
 
 TEAM_ID = "leesah-team-1"
 QUIZ_TOPIC = "quiz-rapid"
 UTF_8 = "utf-8"
 
+Message = Schema({"msg_type": Or("question", "answer")})
+Question = Schema({"msg_type": "question"})
+Answer = Schema({"msg_type": "answer"})
 
-def parse_kafka_message(msg: bytes) -> dict:
+
+def parse_kafka_message_to_json(msg: bytes) -> dict:
     return json.loads(msg.value.decode(UTF_8))
 
 
@@ -20,17 +25,17 @@ if __name__ == "__main__":
     producer = KafkaProducer(bootstrap_servers="localhost:29092")
     for msg in consumer:
         try:
-            parsed_msg = parse_kafka_message(msg)
-            # TODO: use proper schema matching when parsing JSON
-            if parsed_msg["type"] == "question":
-                svar = {"type": "svar", "innhold": "1+1=2"}
+            message = Message.validate(parse_kafka_message_to_json(msg))
+            if Question.is_valid(message):
+                # svar = {"msg_type": "answer", "team": TEAM_ID, "answer": "1+1=2"}
+                svar = {"msg_type": "answer"}
                 print("SVARER MED:", svar)
                 producer.send(QUIZ_TOPIC, bytes(json.dumps(svar), UTF_8))
-            elif parsed_msg["type"] == "svar":
-                print("MOTTOK SVAR:", parsed_msg)
+            elif Answer.is_valid(message):
+                print("MOTTOK SVAR:", message)
             else:
-                print("Fikk inn melding som verken var spørsmål eller svar", parsed_msg)
+                print("Fikk inn melding som verken var spørsmål eller svar", message)
         except json.JSONDecodeError:
             print("Unable to parse JSON:", msg.value)
-        except KeyError as e:
+        except SchemaError as e:
             print("JSON følger ikke skjemaet", msg.value, e)
