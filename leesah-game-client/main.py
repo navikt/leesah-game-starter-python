6146b7db299cf3204c4cbfcee6bd8416248282bd
ulrikah <ulrikhalmoy@gmail.com>
2021-10-14 12:01:12 +0200

    Lagt på eksempelregistrering av teamnavn og gjort flyten mer rigid

--- leesah-game-client/main.py
+++ leesah-game-client/main.py
@@ -1,41 +1,53 @@
 from kafka import KafkaConsumer, KafkaProducer
-from schema import Schema, Or, Use, SchemaError
+from schema import Schema, SchemaError
 import json
 
-TEAM_ID = "leesah-team-1"
+HOST = "localhost:29092"
+CONSUMER_GROUP_ID = "leesah-game-consumer-1"
 QUIZ_TOPIC = "quiz-rapid"
 UTF_8 = "utf-8"
 
-Message = Schema({"msg_type": Or("question", "answer")})
 Question = Schema({"msg_type": "question"})
-Answer = Schema({"msg_type": "answer"})
+Answer = Schema({"msg_type": "answer", "team_id": str, "answer": str})
+Registration = Schema({"msg_type": "registration", "team_id": str})
 
+serialize = lambda value: json.dumps(value).encode(UTF_8)
+deserialize = lambda value: json.loads(value.decode(UTF_8))
 
-def parse_kafka_message_to_json(msg: bytes) -> dict:
-    return json.loads(msg.value.decode(UTF_8))
+###########
 
+TEAM_NAVN = "Team ABC"
+
+###########
 
 if __name__ == "__main__":
     consumer = KafkaConsumer(
-        QUIZ_TOPIC,
-        bootstrap_servers="localhost:29092",
-        group_id=TEAM_ID,
+        bootstrap_servers=HOST,
+        group_id=CONSUMER_GROUP_ID,
         auto_offset_reset="earliest",
     )
-    producer = KafkaProducer(bootstrap_servers="localhost:29092")
-    for msg in consumer:
+    producer = KafkaProducer(
+        bootstrap_servers=HOST,
+    )
+    consumer.subscribe(QUIZ_TOPIC)
+    for kafka_msg in consumer:
         try:
-            message = Message.validate(parse_kafka_message_to_json(msg))
-            if Question.is_valid(message):
-                # svar = {"msg_type": "answer", "team": TEAM_ID, "answer": "1+1=2"}
-                svar = {"msg_type": "answer"}
-                print("SVARER MED:", svar)
-                producer.send(QUIZ_TOPIC, bytes(json.dumps(svar), UTF_8))
+            message = deserialize(kafka_msg.value)
+            if Registration.is_valid(message):
+                print(f"Team {message['team_id']} har blitt registrert")
+            elif Question.is_valid(message):
+                svar = {
+                    "msg_type": "answer",
+                    "team_id": TEAM_NAVN,
+                    "answer": "Dette er svaret på spørsmålet",
+                }
+                producer.send(QUIZ_TOPIC, serialize(svar))
+                print("Svarte med:", svar)
             elif Answer.is_valid(message):
-                print("MOTTOK SVAR:", message)
+                print(f"Mottok svar fra {message['team_id']}: {message['answer']}")
             else:
-                print("Fikk inn melding som verken var spørsmål eller svar", message)
-        except json.JSONDecodeError:
-            print("Unable to parse JSON:", msg.value)
+                raise (SchemaError(f"Meldingsformatet på {message} er ukjent"))
         except SchemaError as e:
-            print("JSON følger ikke skjemaet", msg.value, e)
+            print(e)
+        except (json.JSONDecodeError):
+            print("Fikk ikke til å parse JSON for følgende melding:", kafka_msg.value)
